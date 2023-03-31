f88c2095e6053ccd7aa496d8ede8b8fe4c721ab7
Bernardo <bernardocc@al.insper.edu.br>
2023-03-30 23:16:19 -0300

    aula 10 feita

--- aula10/branchAndBound.cpp
+++ aula10/branchAndBound.cpp
@@ -7,61 +7,40 @@
 
 int n, W, num_leaf, num_copy;
 std::vector<Item> items;
-BackPack answerPack, bestPack;
+BackPack init_pack, bestPack;
 
-BackPack branchAndBound(int i, std::vector<Item> *items, BackPack backPack){
-    if(backPack.capacity <= 0){
-        return backPack;
+void branchAndBound(int i, std::vector<Item> *items, BackPack backPack){
+    if(backPack.capacity < 0){
+        return;
+    }
+    if(backPack.estimatedValue < bestPack.value){
+        return;
+    }
+    if (backPack.value > bestPack.value){
+        bestPack = backPack;
+        num_copy++;
     }
     if(i == 0){
         if(backPack.capacity - items->at(i).weight >= 0){
             backPack.capacity -= items->at(i).weight;
             backPack.value += items->at(i).value;
             backPack.ids.push_back(i);
+            if (backPack.value > bestPack.value){
+                bestPack = backPack;
+                num_copy++;
+            }         
         }
         num_leaf++;
-        return backPack;
+        return;
     }
-    
-    if(backPack.capacity - items->at(i).weight < 0){
-        return bestPack;
-    }
-    else if(backPack.estimatedValue < bestPack.estimatedValue){
-            return bestPack;
-        }
-    else{
-        BackPack m1 = backPack;
-        m1.capacity -= items->at(i).weight;
-        m1.value += items->at(i).value;
-        m1.ids.push_back(i);        
-        branchAndBound(i-1, items, m1);
-        if(backPack.capacity - items->at(i).weight < 0){
-            return branchAndBound(i-1, items, backPack);
-        }
-        if(backPack.estimatedValue < bestPack.estimatedValue){
-            return backPack;
-        }
+    BackPack m1 = backPack;
+    m1.capacity -= items->at(i).weight;
+    m1.value += items->at(i).value;
+    m1.ids.push_back(i);        
+    branchAndBound(i-1, items, m1);
 
-        BackPack m2 = branchAndBound(i-1, items, backPack);
-        if(m1.capacity <= W && m2.capacity <= W){
-            if (m1.value > m2.value){
-                return m1;
-            }
-            else{
-                return m2;
-            }
-        }
-        //mudar conferencias
-        else if (m1.capacity <= W){
-            return m1;
-        }
-        else if (m2.capacity <= W){
-            return m2;
-        }
-        else {
-            return backPack;
-        }
-    }
+    backPack.estimatedValue -= items->at(i).value;
+    branchAndBound(i-1, items, backPack);
 }
 
 int main(){
@@ -73,9 +52,19 @@ int main(){
         items.push_back(temp_item);
     }
 
-    answerPack = branchAndBound(n-1, &items, answerPack);
+    bestPack.capacity = 0;
+    bestPack.value = 0;
 
-    std::cout << answerPack.value << '\n' << answerPack.weight << '\n';
+    init_pack.capacity = W;
+    init_pack.value = 0;
+    init_pack.ids = {};
+    for (int i = 0; i < n; i++){
+        init_pack.estimatedValue += items[i].value;
+    }
+
+    branchAndBound(n-1, &items, init_pack);
+
+    std::cout << "value: " << bestPack.value << '\n' << "Peso: " << W - bestPack.capacity << '\n' << "Quantidade de itens na mochila: " << bestPack.ids.size() << '\n' << "num leaf: " << num_leaf << '\n' << "num copy: " << num_copy << '\n';
 
     return 0;
 }
